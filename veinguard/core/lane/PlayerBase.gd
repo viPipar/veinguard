22080587f54dd249cb9cacd6d7f2f02c828c54ff
farrelalf <farrelalfach@gmail.com>
2026-06-27 14:47:28 +0700

    add hero base

--- veinguard/core/lane/PlayerBase.gd
+++ veinguard/core/lane/PlayerBase.gd
@@ -4,8 +4,20 @@
 class_name PlayerBase
 extends Area2D
 
+@onready var sprite : Sprite2D = $Sprite2D
+
+var _float_time : float = 0.0
+
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
+
+
+func _process(delta: float) -> void:
+	if sprite:
+		_float_time += delta
+		# Efek mengambang (floating) menggunakan gelombang sinus (sin wave)
+		# Naik-turun sejauh 8 pixel dengan kecepatan halus
+		sprite.position.y = sin(_float_time * 2.5) * 8.0
 
 
 func _on_body_entered(body: Node2D) -> void:
